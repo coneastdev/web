460940f035af9391e61c80e6db6a666aac4f15d4
coneastdev <124074325+coneastdev@users.noreply.github.com>
2024-12-07 12:58:58 +0000

    Add files via upload
    Reuploaded geekyclips as github is refuseing to work

---
+++ geekyclips/main.js
@@ -0,0 +1,29 @@
+const product_id =  new URLSearchParams(window.location.search).get('product');
+console.log(product_id);
+if (product_id != null && product_id != "") {
+    console.log(product_id)
+    setTimeout(() => {
+        document.getElementById("shop").style.display = ""
+        document.getElementById("main").style.filter = "blur(3px)"
+        document.getElementById("main").style.filter = "-webkit-filter: blur(3px)"
+        document.getElementById("productname").innerText = product_id
+        document.getElementById("close-btn").style.display = "inline"
+        document.getElementById("name-nav").style.display = "none"
+        document.getElementById("main").style.display = "none"
+        document.getElementById(product_id + "-opt").selected = "true"
+        document.getElementById("product-img").src = "./media/" + product_id + ".png" 
+    }, 100);
+} else {
+    const toastLiveExample = document.getElementById('liveToast')
+}
+
+function selectfunc() {
+    window.location.href = "https://coneastdev.github.io/web/geekyclips?product=" + product_id
+}
+
+
+
+setTimeout(() => {
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+    toastBootstrap.show()
+}, Math.round(Math.random(3000) * 10000));
